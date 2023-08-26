6afa156e88b5e37382263e5380a9160d0d9ab323
Swan <swan.marin@epitech.eu>
2023-08-26 11:53:10 +0200

    finalisation du filtre v2

--- script/controller/filter.js
+++ script/controller/filter.js
@@ -1,38 +1,48 @@
 import RecipeTemplate from '../factory/recipesTemplate.js'
 
 export function filterRecipes(data, tabRecipes, filters) {
-  // console.log(data);
-  // return tabRecipes.filter((recipe) => {
-  const recipesFiltrees = tabRecipes.filter((recipe) => {
-    // Filtrer par la bar de recherche
-    if (data.searchBar && !recipe.name.includes(data.searchBar)) {
-      return false;
-    }
+  // Copie du tableau des recettes pour ne pas modifier l'original
+  const recipesFiltrees = [...tabRecipes];
 
-    // Filtrer par ingrédient
-    if (data.selectedIngredient && !recipe.ingredients.some(ingredients => ingredients.ingredient.includes(data.selectedIngredient))) {
-      return false;
+  // Filtre par la barre de recherche
+  if (data.searchBar) {
+    for (let i = recipesFiltrees.length - 1; i >= 0; i--) {
+      if (!recipesFiltrees[i].name.includes(data.searchBar)) {
+        recipesFiltrees.splice(i, 1);
+      }
     }
-    
-    // Filtrer par appareil
-    if (data.selectedAppliance && !recipe.appliance.includes(data.selectedAppliance)) {
-      return false;
-    }
-    // Filtrer par ustensile
-    if (data.selectedUstensil && !recipe.ustensils.some(ustensil => ustensil.includes(data.selectedUstensil))) {
-      return false;
-    }
+  }
 
-    // Si aucune condition de filtrage ne correspond, la recette est conservée
-    return true;
-  });
-  console.log("Recettes filtrées :", recipesFiltrees);
+  // Filtre par ingrédient
+  if (data.selectedIngredient) {
+    for (let i = recipesFiltrees.length - 1; i >= 0; i--) {
+      if (!recipesFiltrees[i].ingredients.some(ingredient => ingredient.ingredient.includes(data.selectedIngredient))) {
+        recipesFiltrees.splice(i, 1);
+      }
+    }
+  }
+
+  // Filtre par appareil
+  if (data.selectedAppliance) {
+    for (let i = recipesFiltrees.length - 1; i >= 0; i--) {
+      if (!recipesFiltrees[i].appliance.includes(data.selectedAppliance)) {
+        recipesFiltrees.splice(i, 1);
+      }
+    }
+  }
+
+  // Filtre par ustensile
+  if (data.selectedUstensil) {
+    for (let i = recipesFiltrees.length - 1; i >= 0; i--) {
+      if (!recipesFiltrees[i].ustensils.some(ustensil => ustensil.includes(data.selectedUstensil))) {
+        recipesFiltrees.splice(i, 1);
+      }
+    }
+  }
   const $recipesWrapper = document.querySelector('#recipe_section')
   const recipeTemplate = new RecipeTemplate;
   recipesFiltrees.forEach(element => {
     const recipesTemplate = new RecipeTemplate(element)
     $recipesWrapper.appendChild(recipesTemplate.createRecipeCard())
   })
-
-
 }
